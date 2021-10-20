57e37257fc942b0f2021d7fc1c7ebabe4cb1757b
Ritesh-Aldak <riteshaldak13022000@gmail.com>
2021-10-21 03:38:25 +0530

    changed the bgcolor of navbar and also created previous and next button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export class Navbar extends Component {
     render() {
         return (
             <>
-               <nav className="navbar navbar-expand-lg navbar-light bg-light">
+               <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
   <div className="container-fluid">
     <a className="navbar-brand" href="/">NewsHub</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,12 +17,12 @@ export class Navbar extends Component {
           <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">About</a>
+          <a className="nav-link active" href="/">About</a>
         </li>
       </ul>
       <form className="d-flex">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <button className="btn btn-outline-dark" type="submit">Search</button>
       </form>
     </div>
   </div>
--- src/components/News.js
+++ src/components/News.js
@@ -22,20 +22,22 @@ export class News extends Component {
     render() {
         return (
             <div className="container my-1" >
-                <h1 className="my-1" >TOP <span style={{color:'blue'}}>HEADLINES</span></h1>
+                <h1 className="my-1 mx-3" style={{fontFamily:'Pacifico cursive'}}>TOP <span style={{color:'blue', fontFamily:'Pacifico cursive'}}>HEADLINES</span></h1>
                 <div className="row my-2">
                     {this.state.articles.map((element) => {
 
-                        return <div className="col-md-4 my-3" key={element.url}>
-                            <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imgUrl={element.urlToImage} newsUrl={element.url} />
-                            
+                 return <div className="col-md-4 my-3" key={element.url}>
+                            <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imgUrl={element.urlToImage} newsUrl={element.url} /> 
                         </div>
                     }) 
                     }
-                    
-                </div>
-                <hr />
-                </div>
+                    </div>
+                          <hr />
+                          <div className="container d-flex justify-content-between my-5">
+                          <button type="button" className="btn btn-secondary">&#8592;Previous</button>
+                          <button type="button" className="btn btn-secondary">Next&#8594;</button>
+                          </div>
+                    </div>
                 )
     }
 }
